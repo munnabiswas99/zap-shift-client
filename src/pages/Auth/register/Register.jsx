d69d657a90f8734fdf1a9012a92a20d28b9379a1
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-04-11 23:46:05 +0600

    Register form validation done

--- src/pages/Auth/register/Register.jsx
+++ src/pages/Auth/register/Register.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>register</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            {...register("password", { required: true, minLength: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/ })}
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">Password must be atleast 8 character</p>
+          )}
+          {
+            errors.password?.type === 'pattern' && <p className="text-red-500">Password must be at least one digit, one character, special character, upper-case, lower-case.</p>
+          }
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
