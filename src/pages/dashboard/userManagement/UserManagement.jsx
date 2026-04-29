b6a1e2366f7923f123ab57d597b8993c1b3b8bb6
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-04-29 15:36:33 +0600

    Manage user table created

--- src/pages/dashboard/userManagement/UserManagement.jsx
+++ src/pages/dashboard/userManagement/UserManagement.jsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const UserManagement = () => {
-    const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
 
-    const {data: users = []} = useQuery({
-        queryKey: ['users'],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/users`);
-            return res.data;
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users`);
+      return res.data;
+    },
+  });
 
-        }
-    })
-
-    return (
-        <div>
-            <h1>Users: {users.length}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">All Users: {users.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={user.photoURL} alt="Avatar" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.displayName}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default UserManagement;
